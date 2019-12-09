30bd04ead6d3dfcd4df8477e7521617b884e2cb0
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2019-12-08 22:56:18 -0800

    added axiosWithAuth

--- friends-app/src/components/FriendsList.js
+++ friends-app/src/components/FriendsList.js
@@ -1,15 +1,40 @@
-// import React from 'react';
+import React from 'react';
+import axiosWithAuth from '../axiosWithAuth';
+
+class FriendsList extends React.Component {
+    state = {
+        FriendsList: []
+    };
+
+    // componentDidMount() {
+    //     this.getData;
+    // }
+
+    getData = () => {
+        axiosWithAuth()
+            .get("/friends")
+            .then(res => {
+                this.setState({
+                    FriendsList: res.data.friends
+                })
+            })
+            .catch(err => console.log("axiosWithAuth in FriendsList error: ", err));
+    };
+
+    formData = () => {
+        const formattedData = [];
+    }
+
+    render() {
+        return (
+            <div className="friends-list">
+                FriendsList.js
+            </div>
+        )
+    }
 
 
-// const FriendsList = () => {
-//     state = {
-//         friendsList : []
-//     };
-
-//     return (
-//         this.state
-//     )
-// }
+}
 
 
-// export default FriendsList;
+export default FriendsList;
